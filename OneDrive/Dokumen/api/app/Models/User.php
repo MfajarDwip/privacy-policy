0fcbf1d9dc4699ab70778851323d82527593bc35
Muhammad Fajar Dwi Putra <mfajardwip044@gmail.com>
2024-08-07 17:24:51 +0700

    first commit

---
+++ OneDrive/Dokumen/api/app/Models/User.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+class User extends Authenticatable
+{
+    use HasFactory, Notifiable, HasApiTokens;
+    public $timestamps = false;
+
+    protected $fillable = [
+        'email',
+        'password',
+        'role',
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+    
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+
+    protected $table = 'user';
+
+    public function penduduk()
+    {
+        return $this->hasOne(Penduduk::class, 'id_user', 'id');
+    }
+}
